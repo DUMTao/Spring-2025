ff1ba4a169b2d1a10369455fae3ab106fed9127f
Kamil Reyes <ky.reyes2120@gmail.com>
2025-02-15 05:23:44 +0000

    One of his long code problems :v

--- Java Workspace/Labs/Scratch.java
+++ Java Workspace/Labs/Scratch.java
@@ -1,28 +1,34 @@
+import java.util.*;
 
 public class Scratch {
-    public static void main(String[] args) {
-        boolean isPremiumCustomer = false;
-        int nbooksPurchased = 0;
-        int freeBooks;
+    public static void SumDigits(){
+        //Write a java program to read an integer between 0 and 999 and adds all the digits in the integer
+        //Hint: Use the % op to extract digits and / op to remove extracted digit
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Enter your numbers! (Maximum of three): ");
+        
+        int input = terminal.nextInt();
+        String nums = Integer.toString(input);
+        int sum = 0;
+        
+        if (nums.length() > 3){
+            System.out.println("Num too long :(");
+        }
+        else if (nums.length() <= 3){
+            for (int i = 0; i < nums.length(); i++){
+                int digit = nums.charAt(i) - '0';
+                sum += digit;
+            }
+        
+            System.out.println(sum);
+        }
 
-        if (isPremiumCustomer){
-            if (nbooksPurchased == 0){
-                freeBooks = 0;
-            }
-            else if (nbooksPurchased >= 5){
-                freeBooks = 1;
-            }
-            else if (nbooksPurchased >= 8){
-                freeBooks = 2;
-            }
-        }
-        else if (nbooksPurchased >= 7){
-            freeBooks = 1;
-        }
-        else if (nbooksPurchased >= 12){
-            freeBooks = 2;
-        }
-        
-        
+        terminal.close();
+    }
+    
+
+    
+    public static void main(String[] args) {
+        SumDigits();
     }
 }
